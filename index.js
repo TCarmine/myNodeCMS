8fe0537c21fee7d7d57685e4c2f479c07dc39acc
TCarmine <kappa85t@hotmail.it>
2019-12-16 17:59:21 +0100

    created start script, index.js, admin.js, and user model

---
+++ index.js
@@ -0,0 +1,34 @@
+// localhost:27017 default but can be checked with sudo lsof -iTCP -sTCP:LISTEN | grep mongo
+
+//import keystone
+var keystone = require('keystone');
+
+// Set up our keystone instance
+keystone.init({
+  // The name of the KeystoneJS application
+  'name': 'Keystone CMS',
+  // Paths to our application static files
+  'static': [],
+  // Keystone includeed updates framework, 
+  // which can enabled by setting the auto update option to true.
+  // Updates provide an easy way to seed your database, 
+  // transition data when your models change, 
+  // or run transformation scripts against your database.
+  'auto update': true,
+  // The url for your MongoDB connection
+  'mongo': 'mongodb://localhost/allaroundjavascript',
+  // Whether to enable built-in authentication for Keystone's Admin UI,
+  'auth': true,
+  // The key of the Keystone List for users, required if auth is set to true
+  'user model': 'User',
+  // The encryption key to use for your cookies.
+  'cookie secret': '6D61822FBEAED8635A4A52241FEC3',
+});
+
+// Load your project's Models
+keystone.import('./server/models');
+
+// Add routes later 
+
+// Start Keystone
+keystone.start();
