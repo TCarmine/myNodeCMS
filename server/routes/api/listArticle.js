cb11fa4440ccf1cf68b556d24954e3749cf8e4c4
TCarmine <kappa85t@hotmail.it>
2020-01-16 11:12:01 +0100

    some comments typos

--- server/routes/api/listArticle.js
+++ server/routes/api/listArticle.js
@@ -4,7 +4,7 @@ let keystone = require('keystone');
  * List Article
  */
 
-// Getting our recipe model
+// Getting our article model
 let Article = keystone.list('Article');
 
 // Creating the API end point
@@ -14,11 +14,11 @@ exports.list = function (req, res) {
     // Make sure we are handling errors
     if (err) return res.apiError('database error', err);
     res.apiResponse({
-      // Filter recipe by 
+      // Filter articles by 
       article: items,
     });
   
-  // Using express req.query we can limit the number of recipes returned 
+  // Using express req.query we can limit the number of articles returned 
   // by setting a limit property in the link
   // This is handy if we want to speed up loading times once our articles collection 
   // grows
